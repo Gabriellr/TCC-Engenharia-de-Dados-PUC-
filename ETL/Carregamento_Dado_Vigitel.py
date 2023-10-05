bcb373b5ca3326275075cf3e43d15fdf27c1013d
Gabriel Rodrigues <32747916+Gabriellr@users.noreply.github.com>
2023-10-05 15:05:19 -0300

    Alteracao 05 10

--- ETL/Carregamento_Dado_Vigitel.py
+++ ETL/Carregamento_Dado_Vigitel.py
@@ -8,7 +8,7 @@ def load_data_from_api(*args, **kwargs):
     url = 'https://raw.githubusercontent.com/Gabriellr/TCC-Engenharia-de-Dados-PUC-/main/Vigitel-2019-peso-rake.csv'
     url1 = 'https://raw.githubusercontent.com/Gabriellr/TCC-Engenharia-de-Dados-PUC-/main/Vigitel-2020-peso-rake.csv'
     url2 = 'https://raw.githubusercontent.com/Gabriellr/TCC-Engenharia-de-Dados-PUC-/main/Vigitel-2021-peso-rake.csv'      
-
+    url3 = 'https://raw.githubusercontent.com/Gabriellr/TCC-Engenharia-de-Dados-PUC-/main/Vigitel-2018-peso-rake.csv'
     response = requests.get(url)
     urlteste = pd.read_csv(io.StringIO(response.text), sep=';')
 
@@ -17,11 +17,16 @@ def load_data_from_api(*args, **kwargs):
 
     response2 = requests.get(url2)
     urlteste2 = pd.read_csv(io.StringIO(response2.text), sep=';')
+
+    response3 = requests.get(url3)
+    urlteste3 = pd.read_csv(io.StringIO(response3.text), sep=';')
     
     df_atividade_junt1 = pd.merge(urlteste,urlteste1, how = 'outer')
-    df_atividade_junt2 = pd.merge(df_atividade_junt1,urlteste2 , how = 'outer')
+    df_atividade_junt2 = pd.merge(urlteste2,urlteste3, how = 'outer')
+
+    df_atividade_junt3 = pd.merge(df_atividade_junt1,df_atividade_junt2 , how = 'outer')
     
-    return  df_atividade_junt2
+    return  df_atividade_junt3
 
 
 
